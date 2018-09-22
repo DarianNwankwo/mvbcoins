0a191aef62845bb83dd355fbdab0f05e0d5a1746
DarianNwankwo <dnwankwo404@gmail.com>
2018-09-21 23:12:25 -0400

    Sloppy commit msg. Remote dev

--- src/server.py
+++ src/server.py
@@ -134,7 +134,7 @@ class Server(object):
       should_broadcast, block_data = self._handle_data(msg, cur_opcode)
       if should_broadcast and block_data:
         self._broadcast_to_peers(msg)
-        self._broadcast_to_peers(block_data)
+        self._broadcast_to_peers(block_data.raw_byte_array())
       elif should_broadcast:
         self._broadcast_to_peers(msg)
       if cur_opcode == CLOSE_OPCODE:
