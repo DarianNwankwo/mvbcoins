9a878562fbbd389c97760412f6ff23d91e6b3db1
DarianNwankwo <dnwankwo404@gmail.com>
2018-09-21 22:37:31 -0400

    Sloppy commit msg. Remote dev

--- src/transaction.py
+++ src/transaction.py
@@ -55,6 +55,7 @@ class Transaction(object):
     """ Parses the amount and timestamp and returns the integer representation. """
     val = ""
     for i in range(len(ascii_string)//2):
-        val += chr(int(ascii_string[ 2*i : 2*i + 2 ], 16))
+      print("Substring: {}".format(int(ascii_string[ 2*i : 2*i + 2 ], 16)))
+      val += chr(int(ascii_string[ 2*i : 2*i + 2 ], 16))
     print("(TX)Val Inside Parse Ascii: {}".format(val))
     return int(val)
