9a3d4cd7ecfa6e8e57763005f24558a7633c2bee
DarianNwankwo <dnwankwo404@gmail.com>
2018-09-21 22:32:14 -0400

    Sloppy commit msg. Remote dev

--- src/block.py
+++ src/block.py
@@ -54,8 +54,9 @@ class Block(object):
     print("Value of Ascii String: {}".format(ascii_string))
     val = ""
     for i in range(len(ascii_string)//2):
+      print("Substring: {}".format(ascii_string[ 2*i : 2*i + 2 ]))
       val += chr(int(ascii_string[ 2*i : 2*i + 2 ], 16))
-      print("Val Inside Parse Ascii: {}".format(val))
+    print("Val Inside Parse Ascii: {}".format(val))
     return int(val)
 
 
